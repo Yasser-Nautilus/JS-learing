3adae8cec89c1bdba8a707925dca49249d4112ad
Nautilus <yasserbelal2005@gmail.com>
2026-08-02 01:41:26 +0300

    counter project

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-let countEl = document.getElementById("count-el")
-
-console.log(countEl)
-
-let count = 0
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let count = 0;
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  count = 0;
+  countEl.textContent = count;
 }
