3bc952e3b887de11a52c6db58d8c474a6f3360b7
Nautilus <yasserbelal2005@gmail.com>
2026-08-09 08:11:00 +0300

    practice on map

--- Practices/test.js
+++ Practices/test.js
@@ -1,17 +1,21 @@
-const apiResponse = {
-  status: 200,
-  data: {
-    user: {
-      id: 42,
-      profile: { name: "Ali", contacts: ["ali@x.com", "01000000000"] },
-    },
-    permissions: ["read", "write"],
-  },
-};
-const {
-  data: {
-    user: { id, profile: { name, contacts: [userEmail] = [] } = {} } = {},
-    permissions: [readPermission] = [],
-  } = {},
-} = apiResponse;
+const rateLimit = new Map();
 
+function checkRateLimit(userId) {
+  if (!rateLimit.has(userId)) {
+    rateLimit.set(userId, 1);
+    console.log("the user init the first request");
+  } else if (rateLimit.get(userId) < 3) {
+    let numReq = rateLimit.get(userId);
+    numReq += 1;
+    rateLimit.set(userId, numReq);
+    console.log("True");
+  } else if (rateLimit.get(userId) === 3) {
+    console.log("False");
+  }
+}
+checkRateLimit(15);
+checkRateLimit(15);
+checkRateLimit(15);
+checkRateLimit(15);
+
+checkRateLimit(16);
