27cbf8f772473e0d0641e6591f836e29e28a925b
Nautilus <yasserbelal2005@gmail.com>
2026-08-11 22:25:24 +0300

    Date/Time

--- Practices/test.js
+++ Practices/test.js
@@ -1,41 +1,8 @@
-class Employee {
-  #baseSalary;
-  static totalEmployees = 0;
-  constructor(name, baseSalary) {
-    this.name = name;
-    this.#baseSalary = baseSalary;
-    Employee.totalEmployees++;
-  }
-  getSalary() {
-    return this.#baseSalary;
-  }
-  getInfo() {
-    return `${this.name}: ${this.getSalary()}`;
-  }
+const startTime = Date.now();
+
+let sum = 0;
+for (let i = 0; i < 1e7; i++) {
+  sum += i;
 }
-class Manager extends Employee {
-  constructor(name, baseSalary, teamSize) {
-    super(name, baseSalary);
-    this.teamSize = teamSize;
-  }
-  getSalary() {
-    const currentSalary = super.getSalary();
-    return currentSalary + this.teamSize * 500;
-  }
-  getInfo() {
-    const currentInfo = super.getInfo();
-    return currentInfo + " [MANAGER]";
-  }
-}
-
-const e1 = new Employee("Ali", 5000);
-const m1 = new Manager("Sara", 6000, 4);
-
-console.log(e1.getInfo());         // "Ali: $5000"
-console.log(e1.getSalary());        // 5000
-
-console.log(m1.getSalary());        // 8000  (6000 + 4*500)
-console.log(m1.getInfo());          // "Sara: $8000 [MANAGER]"
-console.log(m1.teamSize);            // 4
-
-console.log(Employee.totalEmployees); // 2  (e1 + m1)
+const endTime = Date.now();
+console.log(`Operation took ${endTime - startTime}ms`);
